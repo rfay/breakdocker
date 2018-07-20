6bf07aec33a3359c8d32643425208c2bf45812d2
Randy Fay <randy@randyfay.com>
2018-07-20 07:45:45 -0600

    Add timestamp on exit

--- break46.sh
+++ break46.sh
@@ -4,13 +4,22 @@ set -eu
 set -o pipefail
 
 # 20180719: Broke (failed to connect to docker) on the "stop" of the rm after 46
+# 20180720: Broke during stop of the rm after 20 (with query of container changes in place)
+
+function final {
+	echo "Exit at $(date)"
+}
+
+trap final EXIT
+
 COUNTER=0
 while true;  do
 	for item in d6git d7git d8git; do
 		COUNTER=$[$COUNTER +1]
 		printf "\n\n$COUNTER: $item $(date)\n"
 		cd ~/workspace/$item
-		ddev import-db --src=.tarballs/$item.sql.gz; 
+		ddev import-db --src=.tarballs/$item.sql.gz;
+		ddev snapshot
 		ddev rm -R; 
 		#ddev rm -R --omit-snapshot; 
 	done
